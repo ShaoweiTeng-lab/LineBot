d06b676b51a6411add16dce7f87d5d51e36f7c87
Jason_Teng <sdonline13@gmail.com>
2024-04-13 11:56:31 +0800

    first update

---
+++ src/main/java/com/example/spring_linebot/SpringLineBotApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class SpringLineBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLineBotApplication.class, args);
+    }
+
+}
